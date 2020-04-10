a45d1e56b26d53a758c4ab8d4b92d2d0f1783dd7
OlmLisa <36499020+OlmLisa@users.noreply.github.com>
2020-04-10 12:18:58 +0200

    2.py

---
+++ 2.py
@@ -0,0 +1,80 @@
+## OULMI LISA 3-A blockchain
+
+import re 
+## Exercice 6:
+##email = input("Votre email : ")
+##regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+##if(re.search(regex,email)):  
+##    print("Email OK valide")  
+##else:  
+##    print("Email invalide")  
+
+## Exercice 7:
+##for i in range (0, 10):
+##    print ("un message à l'écran")
+##print ("fin de la boucle")
+
+## Exercice 8:
+##mot = input("votre mot : ")
+##for i in range (0, len(mot)):
+##    lettre = mot[i]
+##    print("Lettre", lettre)
+##print ("fin de la boucle")
+
+## Exercice 9:
+##a = 0
+##b = 10
+##while (a < b):
+##    print(a)
+##    a +=1
+
+## Exercice 10:
+##a = 0
+##b = 10
+##while (b > a):
+##    if b%2 != 0:
+##        print("impair", b)
+##    b -=1
+
+## Exercice 11:
+##votreNombre = input("votre nombre : ")
+##x = 0
+##liste = [1,2,3,4,5, 6, 7, 8, 9,10]
+##for i in range (0, len(liste)):
+##    nombre = liste[i]
+##    if nombre == votreNombre:
+##        print(votreNombre)
+##        x = 1
+##print ("fin de la boucle")
+##if(x == 0):
+##    print("Pas dans l'intervalle")
+
+## Exercice 12:
+##mot = input("votre mot : ")
+##for i in range (0, len(mot)):
+##    lettre = mot[i]
+##    print("Lettre", lettre)
+##print ("fin de la boucle")
+##liste = ["a","d","m"]
+##for lettre in liste:
+##    print (lettre)
+##print ("fin de la boucle")
+
+## Exercice 13:
+##for i in range (0, 15):
+##    if i%3 == 0:
+##      print(i)
+##print ("fin de la boucle")
+
+## Exercice 14:
+##n = input("combien premier n pair: ")
+##nb_max = input("max : ")
+##compteur = 0
+##for i in range (0, int(nb_max)):
+##    if compteur == int(n) :
+##        break
+##    if i%2 == 0:
+##      print(i)
+##      compteur += 1
+##print ("fin de la boucle")
+
