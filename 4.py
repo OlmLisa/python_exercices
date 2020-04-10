85d7ecd2d0c2972784f90499b3e0a86ba2a04fd3
OlmLisa <36499020+OlmLisa@users.noreply.github.com>
2020-04-10 15:04:02 +0200

    4.py

---
+++ 4.py
@@ -0,0 +1,71 @@
+## OULMI LISA 3-A blockchain
+import re
+from math import pi
+## Exercice 21:
+##x = int(input(" x : ")) ## x chaines de caractères que le programme enregistrera
+##fichier = open("data.txt", "x")
+##for i in range (0, x):
+##    fichier.write("Bonjour \n")
+##print ("fin de la boucle")
+##fichier.close()
+
+## Exercice 22:
+##regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+##x = int(input(" x : ")) ## x chaines de caractères que le programme enregistrera
+##fichier = open("data.txt", "a")
+##for i in range (0, x):
+##    fichier.write("lisa.oulmi77@gmail.com\n")
+##print ("fin de la boucle 1")
+##fichier = open("data.txt", "r")
+##for ligne in fichier:
+##     if(re.search(regex,ligne)):
+##         print("Email OK valide")
+##         print (ligne)
+##     else:
+##         print("Email invalide")
+##         print (ligne)
+##print ("fin de la boucle 2")
+##fichier.close()
+
+## Exercice 23:
+##chaine = "bonjour bonjour tout le monde"
+##def compterMots(chaine):
+##    dico={}
+##    listeMots=chaine.split()
+##    print(listeMots)
+##    for mot in listeMots:
+##        if mot in dico:
+##            dico[mot]=dico[mot]+1
+##        else:
+##            dico[mot]=1
+##    print("Fin de la boucle listeMots")
+##    print (dico, '\n')
+##    return dico
+##resultat = compterMots(chaine)
+    
+## Exercice 24:
+##r=float(input('rayon de la sphere : '))
+##def volumeSphere(r):
+##    v=4/3*pi*r**3
+##    return v
+##resultat = volumeSphere(r)
+##print (resultat)
+##print('Le volume de la sphere est {:.2f}'. format(resultat), 'cm3')
+
+## Exercice 25:
+##tuples = (3, 2, 1)
+##print(len(tuples))
+##for i in range (0, len(tuples)):
+##    if i == 0:
+##        un = tuples[i]
+##    elif i == 1:
+##        deux = tuples[i]
+##    else:
+##        trois = tuples[i]
+##print("Fin decompression")
+##def somme(un, deux, trois):
+##    return un + deux + trois
+##resultat = somme(un, deux, trois)
+##print (resultat)
+
+
