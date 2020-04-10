7cd2da62f59e0575352199b97d670b4de9622820
OlmLisa <36499020+OlmLisa@users.noreply.github.com>
2020-04-10 10:48:21 +0200

    add 1.py

---
+++ 1.py
@@ -0,0 +1,49 @@
+## OULMI LISA 3-A blockchain
+
+
+from math import sqrt
+## Exercice 1
+##temps = 6.892
+##distance = 19.7
+##vitesse = distance / temps
+##print("vitesse=", vitesse)
+##print("vitesse = {:.2f}". format(vitesse))
+      
+
+## Exercice 2
+##nom = input("Votre nom : ")
+##age = float(input ("age :"))
+##print ("Nom : ", nom)
+##print ("Age : ", age)
+
+## Exercice 3
+##i = float(input("Votre flottant : "))
+##if (i < 0 ):
+##    print("Erreur le flottant doit être >=0.")
+##else:
+##    y= sqrt(i)
+##    print("La racine de", i, "est", y)
+##print("Au revoir.")
+
+## Exercice 4
+##mot1 = input("Votre mot1 : ")
+##mot2 = input ("Votre mot2 : ")
+##if mot1 < mot2:
+##    plus_petit = mot1
+##else :
+##    plus_petit = mot2
+##print(plus_petit)
+
+## Exercice 5
+##pSeuil = 2.3
+##vSeuil = 7.41
+##pression = float(input("Pression : "))
+##volume = float(input("Volume : "))
+##if(pression > pSeuil) and (volume > vSeuil):
+##    print("arrêt immédiat")
+##elif pression > pSeuil:
+##    print("augmenter le volume de l'enceinte")
+##elif volume > vSeuil:
+##    print("diminuer le volume")
+##else:
+##    print ("tout va bien")
